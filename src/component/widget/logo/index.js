7448a89f47d96c6203062fc2736d1dfd604fc511
Oluwatomisin <malcolmtomisin@gmail.com>
2020-05-22 05:05:17 +0100

    link assets

---
+++ src/component/widget/logo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { LogoView } from './styles';
+
+const Logo = (props) => (<LogoView {...props} />);
+
+export default Logo;
+
---
+++ src/component/widget/logo/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native';
+
+export const LogoView = styled.Image`
+width: 50px;
+height: 50px;`;
