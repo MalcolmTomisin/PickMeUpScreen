0694b03e077870968c7e148627e55d02a4be4493
Oluwatomisin <malcolmtomisin@gmail.com>
2020-05-22 04:58:19 +0100

    logo

--- src/component/view/auth/login.js
+++ src/component/view/auth/login.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import { TextView, ContainerView } from './styles';
+import { TextView, ContainerView,LogoView } from './styles';
 
 const login = () => {
   return (
     <ContainerView>
+      <LogoView source={require('../../../assets/logo.png')}/>
       <TextView>login Screen</TextView>
     </ContainerView>
   );
--- src/component/view/auth/styles.js
+++ src/component/view/auth/styles.js
@@ -1,8 +1,14 @@
 import styled from 'styled-components/native';
 import Text from '../../widget/text';
+import Logo from '../../widget/logo';
 
 const TextView = styled(Text)`
 text-align: center;
+`;
+
+const LogoView = styled(Logo)`
+margin-top: 10%;
+margin-left: 5%;
 `;
 
 const ContainerView = styled.View`
@@ -11,4 +17,4 @@ background-color: #0993f3;
 flex: 1;
 `;
 
-export { TextView, ContainerView };
+export { TextView, ContainerView,LogoView };
